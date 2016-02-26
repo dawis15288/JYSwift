7b98ae23ad8634a122f19ed749ca09ef2bc066b7
dawis15288 <cyin15288@hotmail.com>
2016-02-26 20:58:10 +0800

    JYHTML

---
+++ JYHTML/JYHTMLSimpleProject/JYHTMLSimpleProject/JYCell.swift
@@ -0,0 +1,15 @@
+//
+//  JYCell.swift
+//  JYHTMLSimpleProject
+//
+//  Created by atom on 16/2/21.
+//  Copyright © 2016年 cyin. All rights reserved.
+//
+
+import UIKit
+
+class JYCell: UICollectionViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+}
